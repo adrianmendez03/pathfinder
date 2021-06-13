f557e09b0828b66e84c6bd462f831dd6e22d803e
Adrian Mendez <adrianmndz328@gmail.com>
2021-06-13 16:25:24 -0500

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Grid from "./components/grid"
+
+const App: React.FC = () => {
+  return (
+    <div>
+      App
+      <Grid />
+    </div>
+  )
+}
+
+export default App
